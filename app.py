b439e3e7ff57f19a74bc7d8ab90fddb3ff147360
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-10-21 19:36:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = pickle.load(open('./model.pkl', 'rb'))
 
 def run():
     img1 = Image.open('bank_loan.png')
-    img1 = img1.resize((400, 200))
+    img1 = img1.resize((160, 120))
     st.image(img1,use_column_width=False)
     st.title("Bank Loan Prediction using Machine Learning")
 
